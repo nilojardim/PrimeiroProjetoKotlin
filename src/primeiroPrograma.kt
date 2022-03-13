d88cc36debcc57fbba963de3c21a2a982a839d27
nilojardim <97597953+nilojardim@users.noreply.github.com>
2022-03-13 10:33:43 -0300

    o que são variáveis e seus tipos

---
+++ src/primeiroPrograma.kt
@@ -0,0 +1,30 @@
+        fun main () {
+            //Inteiro (1, 2, 3, 4) (int)
+            val anoAtual = 2020
+            val proximoAno: Int = 2021
+
+            //Float (1.5, 2.7, 5.6)
+            val altura = 1.77f
+            val pesoDaCadeira: Float = 4.5f
+
+            //Double > Float
+            val PI: Float = 3.14159265359f
+
+            //Char
+            val primeiraLetraDoMeuNome: Char = '@'
+
+            //String - Fio
+            val nome = "Nilo"
+            val sobrenome: String = "Martins"
+
+            //Booleans
+            val voceEhAlto = false
+            val voceTrabalha: Boolean = true
+
+            val ehMaior = 3 == 3
+            println(ehMaior)
+
+
+
+
+        }
