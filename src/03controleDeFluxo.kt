17c5c59620812e47a341d1241128373a759b6853
nilojardim <97597953+nilojardim@users.noreply.github.com>
2022-03-14 23:28:33 -0300

    Controle de fluxo: Expressão if/else
    Estudos

--- src/03controleDeFluxo.kt
+++ src/03controleDeFluxo.kt
@@ -7,7 +7,7 @@
         }*/
 
 
-          /*val hora = 18
+        /*val hora = 18
                 if (hora < 12) {
                 print ("Bom dia")
             }   else if (hora >= 12 && hora < 18) {
@@ -28,16 +28,38 @@
         //maior = if (a > b) a else b
 
 
-        val antonia = 1
+        /*val antonia = 1
 
             if (antonia > 10) {
                 println("O Aslan é meu")
             } else if (antonia > 0 && antonia <= 10){
                 println ("vou ter que vender o aslan")
             } else
-                println ("Pô, ela nem filhote teve e vc quer o que?")
+                println ("Pô, ela nem filhote teve e vc quer o que?")*/
+
+
+        /*val kill = 21
+
+        if (kill >= 5 && kill <= 20) {
+            println("Noob")
+        } else if (kill < 5) {
+            println("desinstala o bf")
+        } else
+            println("lvl 500")*/
+
+
+        val number = -50
+
+        if (number > 0) {
+            println ("Positive number")
+        } else
+            println("Positive number not found")
+
+    }
 
 
 
 
-      }
+
+
+
